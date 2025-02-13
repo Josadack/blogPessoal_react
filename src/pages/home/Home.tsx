77da56991baaf8fe693a5c61b0d888730c14c717
Josadack <josadaqueferreira@outlook.com>
2025-02-12 21:06:42 -0300

    :tada: Primeiro commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,37 @@
+
+
+function Home() {
+  return (
+    <div style={{
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+    }}>
+        <div>
+            <div style={{
+                width: "80vw",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+            }}>
+                <h2>Seja bem Vindo</h2>
+                <p>Expresse aqui seus pensamentos e opinioes</p>
+
+                <div style={{
+                width: "80vw",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+            }}>
+                <img
+                    src="https://i.imgur.com/VpwApCU.png"
+                    alt="Imagem da pagina "
+                    width="400px"/>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
